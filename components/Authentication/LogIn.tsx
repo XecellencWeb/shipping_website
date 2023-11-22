65d35ced3925d3a798b62b47f555994491aad312
XecellencWeb <excellencejosiah@gmail.com>
2023-11-22 17:25:38 +0100

    new update

--- components/Authentication/LogIn.tsx
+++ components/Authentication/LogIn.tsx
@@ -79,7 +79,7 @@ if(loggedInUser){
 
   return (
 
-    <div className='w-screen h-[500vh] fixed top-0 left-0 isolate'>
+    <div className='w-screen h-[500vh] absolute top-0 left-0 isolate z-50'>
       <div className="bg-black opacity-30 w-full h-full blur-lg absolute top-0 left-0 -z-10"/>
       <div className="wrapper min-h-[70vh] rounded-[1rem] bg_background absolute mt-20 top-0 left-1/2 -translate-x-1/2">
         <form onSubmit={sumbitForm} action="" className="w-96 flex mt-20 flex-col gap-2 mx-auto">
