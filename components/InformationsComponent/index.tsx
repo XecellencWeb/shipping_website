478f2df3cfcd7711c133b4d53984d1664a672ff1
XecellencWeb <excellencejosiah@gmail.com>
2023-11-22 17:12:37 +0100

    changes made

--- components/InformationsComponent/index.tsx
+++ components/InformationsComponent/index.tsx
@@ -14,12 +14,12 @@ const index = () => {
       {
                 Services.map(
                   (service,index)=>(
-                    <div key={index} className="flex w-full gap-2 bg-white bg-opacity-20 p-8 rounded-[1rem] text-white">
-                      <i className={service.emoji + ' text-[1.2rem] text-white'}>
+                    <div key={index} className="flex w-full gap-2 bg-white bg-opacity-20 p-8 rounded-[1rem">
+                      <i className={service.emoji + ' text-[1.2rem] '}>
                       </i>
                       <div className="">
-                        <h1 className="text-[2rem] text-white">{service.name}</h1>
-                        <p className="text_1 mt-2 text-white">{service.description}</p>
+                        <h1 className="text-[2rem] ">{service.name}</h1>
+                        <p className="text_1 mt-2 ">{service.description}</p>
                         
                       </div>
                     </div>
