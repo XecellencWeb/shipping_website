687265f096be10017f3f94f3f1ab5371744b9fcb
XecellencWeb <excellencejosiah@gmail.com>
2023-11-28 11:23:07 +0100

    new changes

--- components/GettingStarted/index.tsx
+++ components/GettingStarted/index.tsx
@@ -11,7 +11,7 @@ const GettingStarted = () => {
 
   return (
     shrinker?
-    <div className='sticky bottom-6 w-32 h-32 flex items-center justify-center rounded-full border-2 border-slate-300 border-opacity-50'>
+    <div className='sticky -bottom-5 w-32 h-32 flex items-center justify-center rounded-full border-2 border-slate-300 border-opacity-50'>
         <div className="absolute -top-16 left-1/2 flex gap-1 items-center">
         <i className="bi bi-play-circle text-5xl"></i>
             <Link href='/calculateshipping' className='px-4 py-2 bg-slate-50 bg-opacity-50  text-base block w-32  m-4 rounded-lg'>Get Started</Link>
@@ -24,10 +24,10 @@ const GettingStarted = () => {
         <button onClick={()=>setShrinker(false)} className="flex items-center justify-center w-10 h-10 rounded-full bg-blue-500"><p className="">x</p></button>
     </div>
     :(
-        <div className='sticky bottom-6 w-32 h-32 flex items-center justify-center rounded-full'>
+        <div className='sticky -bottom-5 w-32 h-32 flex items-center justify-center rounded-full'>
         
         <button onClick={()=>setShrinker(true)} className="flex items-center justify-center px-4 py-2 rounded-full bg-blue-500">
-        <p className="text-base">Get Started</p>
+        <p className="text-base text-white">Get Started</p>
         </button>
     </div>
     )
